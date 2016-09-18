7a3ae062664071e6299e5c78474e6df10b761e82
Joseph Wynn <joseph.wynn@bbc.co.uk>
2016-09-18 18:58:22 +0100

    rows n cols

--- tests/maze_test.rb
+++ tests/maze_test.rb
@@ -16,7 +16,14 @@ eof
 end
 
 class MazeTest < Minitest::Test
-    def test_parse_creates_a_maze
-        assert_instance_of Maze, Maze.parse(test_maze)
-    end
+  def test_parse_creates_a_maze
+    assert_instance_of Maze, Maze.parse(test_maze)
+  end
+
+  def test_parse_creates_grid
+    maze = Maze.parse(test_maze)
+
+    assert_equal 5, maze.cols
+    assert_equal 7, maze.rows
+  end
 end
